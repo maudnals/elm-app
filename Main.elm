a58ebef3556fc865cf0deb30a1e2a6e76c1b01d5
D068996 <m.nalpas@sap.com>
2018-04-11 20:35:24 +0200

    added base and config files

---
+++ Main.elm
@@ -0,0 +1,22 @@
+module Main exposing (..)
+import Html exposing (div, Html, program, text)
+
+
+
+-- MODEL
+
+type alias Model =
+  String
+
+
+
+-- MESSAGE
+
+type Msg = 
+  NoOp
+
+
+init : (Model, Cmd Msg)
+init 
+
+
